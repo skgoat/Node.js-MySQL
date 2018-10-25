f647fdb20ffd3f3d84f59aca0ca8c5998f4c19bd
skgoat <kellysp11@gmail.com>
2018-10-24 23:41:07 -0400

    add final

--- bamazon.sql
+++ bamazon.sql
@@ -4,13 +4,12 @@ CREATE database bamazonDB;
 USE bamazonDB;
 
 CREATE TABLE products (
-  id INT NOT NULL AUTO_INCREMENT,
-  product_name VARCHAR(100) NULL,
-  department_name VARCHAR(100) NULL,
-  price DECIMAL(10,2) NULL,
-  stock_quantity INT NULL,
-
-  PRIMARY KEY (id)
+  item_id INT(10) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(100) NOT NULL,
+  department_name VARCHAR(100) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT (100) NOT NULL,
+  PRIMARY KEY (item_id)
 );
 
 INSERT INTO products (product_name, department_name, price, stock_quantity)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -4,9 +4,7 @@ var inquirer = require("inquirer");
 // create the connection information for the sql database
 var connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
-  port: 3000,
+  port: 3306,
 
   // Your username
   user: "root",
@@ -16,9 +14,112 @@ var connection = mysql.createConnection({
   database: "bamazonDB"
 });
 
-// connect to the mysql server and sql database
-connection.connect(function(err) {
-  if (err) throw err;
-  // run the start function after the connection is made to prompt the user
-  start();
-});
+function validateInput(value) {
+	var integer = Number.isInteger(parseFloat(value));
+	var sign = Math.sign(value);
+
+	if (integer && (sign === 1)) {
+		return true;
+	} else {
+	return 'Please enter a number above.';
+	}
+}
+function promptUserPurchase() {
+	inquirer.prompt([
+{
+	type: 'input',
+	name: 'item_id',
+	message: 'Enter the Item ID which you would like to buy.',
+	validate: validateInput,
+	filter: Number
+		},
+	{
+		type: 'input',
+		name: 'quantity',
+		message: 'How many would you like?',
+		validate: validateInput,
+		filter: Number
+	}
+	]).then(function(input) {
+
+		var item = input.item_id;
+		var quantity = input.quantity;
+
+
+		var storeitems = 'SELECT * FROM products WHERE ?';
+
+		connection.query(storeitems, {item_id: item}, function(err, data) {
+			if (err) throw err;
+
+	
+			if (data.length === 0) {
+				console.log('ERROR: Invalid Item ID.');
+				displayInventory();
+
+			} else {
+				var productData = data[0];
+
+				if (quantity <= productData.stock_quantity) {
+					console.log('Congratulations, the product is in stock! ORDERED!');
+
+					// Construct the updating query string
+					var updatestoreitems = 'UPDATE products SET stock_quantity = ' + (productData.stock_quantity - quantity) + ' WHERE item_id = ' + item;
+				
+	connection.query(updatestoreitems, function(err, data) {
+		if (err) throw err;
+
+						console.log('Your order was placed! Your total is $' + productData.price * quantity);
+						console.log('Thank you for your business!');
+						console.log("\n---------------------------------------------------------------------\n");
+
+						// End the database connection
+						connection.end();
+					})
+				} else {
+					console.log('Insufficent quantity! Change your order!');
+					console.log('Please modify your order.');
+					console.log("\n---------------------------------------------------------------------\n");
+
+					displayInventory();
+				}
+			}
+		})
+	})
+}
+
+function displayInventory() {
+
+	storeitems = 'SELECT * FROM products';
+
+	connection.query(storeitems, function(err, data) {
+		if (err) throw err;
+
+		console.log('Existing Inventory: ');
+		console.log('...................\n');
+
+		var itemsforsale = '';
+		for (var i = 0; i < data.length; i++) {
+			itemsforsale = '';
+			itemsforsale += 'Item ID: ' + data[i].item_id + '  |  ';
+			itemsforsale += 'Product Name: ' + data[i].product_name + '  |  ';
+			itemsforsale += 'Department: ' + data[i].department_name + '  |  ';
+			itemsforsale += 'Price: $' + data[i].price + '\n';
+
+			console.log(itemsforsale);
+		}
+
+	  	console.log("---------------------------------------------------------------------\n");
+
+	
+	  	promptUserPurchase();
+	})
+}
+
+
+function runBamazonDB() {
+	
+	displayInventory();
+}
+
+// Run the application logic
+runBamazonDB();
